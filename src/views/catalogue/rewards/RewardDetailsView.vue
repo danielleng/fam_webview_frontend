c71757f37f5c1facd6821574fa4cecbaf1eae976
Daniel Leng <danlengjj@gmail.com>
2023-01-29 16:55:06 +0800

    Add new dynamic route for Catalogue / Rewards pages

---
+++ src/views/catalogue/rewards/RewardDetailsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="px-4"> 
+    <div class="grid gap-4 grid-cols-3">
+      <div class="card">Catalogue / Rewards / {{ $route.params.id }}</div>
+    </div>
+
+    <hr class="mt-6"/>
+  </main>
+</template>
