a699667cfc42895fd91dd23329ed62f3a03771f6
Kunquest <iamjasonkung@gmail.com>
2023-02-02 22:21:19 +0800

    update referral and services screen

--- src/views/ReferralView.vue
+++ src/views/ReferralView.vue
@@ -5,50 +5,45 @@ import referralShareIcon from '../assets/referral_screen/icons_share_gray.svg'
 
 <template>
     <body>
-        <div class="container mx-auto">
-            <div class="flex flex-col">
-                <div class="overflow-hidden bg-white">
-                    <ul role="list" class="">
-                        <li class="px-5 pt-6 pb-2">
-                            <div class="text-2xl font-semibold text-gray-700">Refer &amp; Earn Points!</div>
-                        </li>
-                        <li class="px-2 py-2">
-                            <img :src="referralHeroImg" class="rounded-2xl"/>
-                        </li>
-                        <li class="px-5 py-2">
-                            <div class="text-base text-gray-700 flex flex-row px-4 py-3 rounded-lg border-2 border-gray-500">
-                                <div class="flex-1">
-                                    <div class="text-sm">My Referral Code</div>
-                                    <div class="text-xl font-semibold">JDL01823E</div>
-                                </div>
-                                <div class="self-center">
-                                    <img :src="referralShareIcon" class="w-6" />
-                                </div> 
-                            </div>
-                        </li>
-                        <li class="px-5 py-2">
-                            <div class="text-sm text-gray-700">
-                                <p>Both you and your friend gets a $5 ShopFarEast E-Voucher upon successful signup with your code!.</p>
-                                <br>
-                                <p><u>How It Works</u>
-                                Tap the code above and send it to your friend.<br>
-                                Your friend will need to tap the link to download the app.<br>
-                                Once the app is downloaded enter the code during the sign up process.<br>
-                                Upon successful registration you and your friend will both receive a $5 ShopFarEast E-Voucher!<br></p>
-                                <br>
-                                <p><u>Terms & Conditions</u>
-                                Terms Line<br>
-                                Terms Line<br>
-                                Terms Line<br>
-                                Terms Line<br></p>
-                            </div>
-                        </li>
+        <div class="container mx-auto mb-28">
+            <ul role="list" class="">
+                <li class="px-5 pt-6 pb-2">
+                    <div class="text-2xl font-semibold text-gray-700">Refer &amp; Earn Points!</div>
+                </li>
+                <li class="px-2 py-2">
+                    <img :src="referralHeroImg" class="rounded-2xl"/>
+                </li>
+                <li class="px-5 py-2">
+                    <div class="text-base text-gray-700 flex flex-row px-4 py-3 rounded-lg border-2 border-gray-500">
+                        <div class="flex-1">
+                            <div class="text-sm">My Referral Code</div>
+                            <div class="text-xl font-semibold">JDL01823E</div>
+                        </div>
+                        <div class="self-center">
+                            <img :src="referralShareIcon" class="w-6" />
+                        </div> 
+                    </div>
+                </li>
+                <li class="px-5 py-2">
+                    <div class="text-sm text-gray-700">
+                        <p>Both you and your friend gets a $5 ShopFarEast E-Voucher upon successful signup with your code!.</p>
+                        <br>
+                        <p><u>How It Works</u><br>
+                        Tap the code above and send it to your friend.<br>
+                        Your friend will need to tap the link to download the app.<br>
+                        Once the app is downloaded enter the code during the sign up process.<br>
+                        Upon successful registration you and your friend will both receive a $5 ShopFarEast E-Voucher!<br></p>
+                        <br>
+                        <p><u>Terms & Conditions</u><br>
+                        Terms Line<br>
+                        Terms Line<br>
+                        Terms Line<br>
+                        Terms Line<br></p>
+                    </div>
+                </li>
 
-                        <!-- More items... -->
-                    </ul>
-                </div>
-                
-            </div>
+                <!-- More items... -->
+            </ul>
         </div>
     </body>
   </template>
--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,65 +1,192 @@
 <script setup>
-import referralHeroImg from '../assets/referral_screen/referral_hero_image@3x.jpg'
+import shortcutButtonImg from '../assets/services_screen/shortcutbutton@3x.jpg'
+
 import "https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"
 </script>
 
 <template>
     <body>
-        <div class="container mx-auto">
-            <div class="flex flex-col">
-                <div class="mx-2 my-4">
+        <div class="container mx-auto mb-28">
+            <ul role="list" class="">
+                <li class="px-2">
                     <lottie-player src="https://assets8.lottiefiles.com/datafiles/tsAvARLZVdV2dUl/data.json"  background="transparent"  speed="1"  style="width: 200; height: 150;"  loop  autoplay></lottie-player>
-                </div>
-                <div class="mx-7 mt-2 mb-10">
-                    <nav class="mt-5 space-y-1 px-2">
-                        <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-                        <a href="/home" class=" text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                            <!--
-                                Heroicon name: outline/home
+                </li>
+                <li class="px-5 pt-2 pb-2">
+                    <div class="text-lg font-regular text-gray-700">Recently Used</div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="px-5 pt-2 pb-2">
+                    <div class="text-lg font-regular text-gray-700">My Account</div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="px-5 pt-2 pb-2">
+                    <div class="text-lg font-regular text-gray-700">Shopping Rewards</div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="px-5 pt-2 pb-2">
+                    <div class="text-lg font-regular text-gray-700">Office Tower</div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="px-5 pt-2 pb-2">
+                    <div class="text-lg font-regular text-gray-700">Convention Centre</div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex">
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                        <div class="text-center w-1/4 px-1">
+                            <a href="">
+                                <img :src="shortcutButtonImg" class="w-16 mx-auto pb-1" />
+                                <div class="text-xs">Test title goes here</div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                
 
-                                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                            -->
-                            <svg class="text-gray-500 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                            Home
-                        </a>
-                        <a href="/wallet" class=" text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                            <!--
-                                Heroicon name: outline/home
-
-                                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                            -->
-                            <svg class="text-gray-500 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                            Wallet
-                        </a>
-                        <a href="/scan" class=" text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                            <!--
-                                Heroicon name: outline/home
-
-                                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                            -->
-                            <svg class="text-gray-500 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                            Scan
-                        </a>
-                        <a href="/services" class=" text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                            <!--
-                                Heroicon name: outline/home
-
-                                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                            -->
-                            <svg class="text-gray-500 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                            Services
-                        </a>
-                    </nav>
-                </div>
-            </div>
+                <!-- More items... -->
+            </ul>
+                
         </div>
     </body>
   </template>
